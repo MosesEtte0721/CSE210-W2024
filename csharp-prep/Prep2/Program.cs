71984823b2e609bfc11eda075bd80349fe1c7e76
Moses <ett20002@byui.edu>
2024-01-11 17:41:34 +0100

    first modification

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,29 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter scores: ");
+        int input = int.Parse(Console.ReadLine());
+        if (input >= 90)
+        {
+            Console.WriteLine("Your grade is A");
+        }
+        else if (input >= 80)
+        {
+            Console.WriteLine("Your grade is B");
+        }
+        else if (input >= 70)
+        {
+            Console.WriteLine("Your grade is C");
+        }
+        else if (input >= 60)
+        {
+            Console.WriteLine("Your grade is D");
+        }
+        else if (input < 60)
+        {
+            Console.WriteLine("Your grade is F");
+        }
+
+
     }
 }
