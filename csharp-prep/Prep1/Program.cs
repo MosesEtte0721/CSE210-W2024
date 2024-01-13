0b5465b420fa178e3070df27f8a43f2eb48885a4
Moses <ett20002@byui.edu>
2024-01-13 22:44:44 +0100

    String interpolation

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,6 +5,21 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
-        Console.WriteLine("I am back!");
+// first name prompt
+        Console.Write("What is your First Name? ");
+        string firstName =  Console.ReadLine();
+// last name prompt
+        Console.Write("What is your Last Name ");
+        string lastName = Console.ReadLine();
+        
+        if (lastName.Length == 0 || firstName.Length == 0)
+        {
+            Console.WriteLine("Empty. Please enter Name");
+        } else {
+            Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
+
+        }
+
+
     }
 }
