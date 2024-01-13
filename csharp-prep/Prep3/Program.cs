f101a90a9565ee8f18d4a845a016a5e1f97941d5
Moses <ett20002@byui.edu>
2024-01-14 00:00:29 +0100

    while loop and generating random number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,41 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        Random randNumber = new Random();
+        int num = randNumber.Next(1, 101);
+        Console.WriteLine($"Magic number is: {num}");
+
+        Console.Write("Type the magic number: ");
+        int userInput = int.Parse(Console.ReadLine());
+
+        if (userInput > num)
+        {
+            Console.WriteLine("Guess Lower");
+        }
+        else if (userInput < num)
+        {
+            Console.WriteLine("Guess Higher");
+        }
+        else{
+        }
+
+        while (userInput != num)
+        {
+            if (userInput > num)
+            {
+                Console.WriteLine("Guess Lower");
+            }
+            else if (userInput < num)
+            {
+                Console.WriteLine("Guess Higher");
+            }
+            Console.Write("What is your guess? ");
+            userInput = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Congrats! Your guess is correct");
+
+        
+
     }
 }
