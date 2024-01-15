7499213e51edaeb57f32594973f1b4dae62b7e0e
Moses <ett20002@byui.edu>
2024-01-15 13:35:14 +0100

    function and function call

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -5,5 +5,47 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine();
+
+        DisplayResult();
+
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program");
+        Console.WriteLine();
+
+    }
+
+    static string PromptUserName()
+    {
+        Console.WriteLine("Enter your name: ");// prompt for the username
+        string userName = Console.ReadLine();
+        Console.WriteLine();
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Enter your favourite number (an integer): "); // prompt for a number
+        int userNumber;
+        // converts the user input to an integer
+        int.TryParse(Console.ReadLine(), out userNumber);
+        Console.WriteLine();
+        return userNumber;
+    }
+
+    static int SquaredNumber(int number)
+    {
+        return number * number;
+    }
+
+    static void DisplayResult()
+    {
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        Console.WriteLine($"{userName}, the square of your number is: {SquaredNumber(userNumber)}");
     }
 }
