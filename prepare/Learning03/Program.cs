bc713fb8f6d2ac7d0b88e28743e8bad83693b075
Moses <ett20002@byui.edu>
2024-01-27 18:55:58 +0100

    encapsulation learning 2

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,29 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+
+        Fraction oneOne = new Fraction();
+        Fraction topFive = new Fraction(5);
+        Fraction topBottom = new Fraction(5,5);
+
+        // oneOne.GetTop();
+        Console.WriteLine($"{oneOne.GetFractionString()}");
+        Console.WriteLine($"{oneOne.GetDecimalValue()}");
+        Console.WriteLine();
+
+        // topFive.GetTop();
+        Console.WriteLine($"{topFive.GetFractionString()}");
+        Console.WriteLine($"{topFive.GetDecimalValue()}");
+        Console.WriteLine();
+
+        
+        topBottom.SetBottom(3);
+        topBottom.SetTop(19);
+        topBottom.GetTop();
+        Console.WriteLine($"{topBottom.GetFractionString()}");
+        Console.WriteLine($"{topBottom.GetDecimalValue()}");
+
+
+
     }
 }
